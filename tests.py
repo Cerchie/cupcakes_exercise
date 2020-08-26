2034cc4b70259dcbdc18c5008f879f3c89e109f6
Lucia Cerchie <luciacerchie@gmail.com>
2020-08-26 14:19:41 -0700

    fourth test not running

--- tests.py
+++ tests.py
@@ -28,6 +28,13 @@ CUPCAKE_DATA_2 = {
     "image": "http://test.com/cupcake2.jpg"
 }
 
+CUPCAKE_DATA_3 = {
+    "flavor": "TestFlavor3",
+    "size": "TestSize3",
+    "rating": 1,
+    "image": "http://test.com/cupcake2.jpg"
+}
+
 
 class CupcakeViewsTestCase(TestCase):
     """Tests for views of API."""
@@ -107,3 +114,36 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+
+def test_patch_cupcake(self):
+    with app.test_client() as client:
+        url = "/api/cupcakes/<int:id>"
+        resp = client.post(url, json=CUPCAKE_DATA_3)
+
+        self.assertEqual(resp.status_code, 200)
+
+        data = resp.json
+
+        # don't know what ID we'll get, make sure it's an int & normalize
+        self.assertIsInstance(data['cupcake']['id'], int)
+        del data['cupcake']['id']
+
+        self.assertEqual(data, {
+            "cupcake": {
+                "flavor": "TestFlavor3",
+                "size": "TestSize3",
+                "rating": 1,
+                "image": "http://test.com/cupcake2.jpg"
+            }
+        })
+
+        self.assertEqual(Cupcake.query.count(), 2)
+
+
+# def test_delete_cupcake(self):
+#     with app.test_client() as client:
+#         url = "/api/cupcakes/<int:id>"
+#         resp = client.post(url, json=CUPCAKE_DATA_3)
+
+#         self.assertEqual(resp.status_code, )
